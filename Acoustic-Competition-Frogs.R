28a0567f620b265ef7edd8a8700623a31a2e4907
Burchardt <Lara.Burchardt@mfn-berlin.de>
2020-11-26 16:25:05 +0100

    new package for stats, new stats

--- Acoustic-Competition-Frogs.R
+++ Acoustic-Competition-Frogs.R
@@ -7,20 +7,33 @@
 # 00: Load Packages --------------------------------------------------------------------------
 
 library(tidyverse)
+library(PMCMR)
+library(PMCMRplus)
 
 # 01: Load Data ------------------------------------------------------------------------------
 
 # IOI Data
 
 ioi <- read_delim("all_IOIs_WSF_ESF.csv", delim = ";")
+ioi <- ioi %>% 
+  as.data.frame() %>% 
+  mutate(group = as.factor(group))
+  
 
 
 #02: Stats -----------------------------------------------------------------------------------
 
-# WSF iois: 1298
-# ESF iois: 876
-# WSF when ESF present iois: 847
-# ESF when WSF present iois: 945 
+summary(ioi)
+
+# WSF iois: max = 55.69; n = 1298
+# ESF iois: max = 38.86, n =  876
+# WSF when ESF present iois: max = 24.22, n =  847
+# ESF when WSF present iois: max = 22.18, n = 945
+
+
+kruskal.test(ioi~group, data = ioi)
+test1 <- posthoc.kruskal.dunn.test(ioi$ioi, ioi$group, p.adjust.method = "bonferroni") #bonferroni most conservative correction; other p.adjust methods: "holm", "none", "BH" ...
+test1
 
 # 03: Histograms -----------------------------------------------------------------------------
 
@@ -31,4 +44,7 @@ hist(ioi$ESF)
 hist(ioi$WSF_ESF_present)
 hist(ioi$ESF_WSF_present)
 
+#quick and dirty boxplot
+
+boxplot(ioi~group, data = ioi)
 
